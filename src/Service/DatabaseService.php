f17582acf856e6e2fa6ae80100dea5417c051ed1
Starozytny <starozytny@hotmail.fr>
2021-11-10 21:25:44 +0100

    service to get windev em

--- src/Service/DatabaseService.php
+++ src/Service/DatabaseService.php
@@ -6,15 +6,19 @@ namespace App\Service;
 
 use Doctrine\DBAL\DBALException;
 use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
 class DatabaseService
 {
     private $em;
+    private $emWindev;
 
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(ManagerRegistry $registry)
     {
-        $this->em = $entityManager;
+        $this->em = $registry->getManager('default');
+        $this->emWindev = $registry->getManager('windev');
     }
 
     public function resetTable(SymfonyStyle $io, $list)
@@ -28,5 +32,15 @@ class DatabaseService
             $this->em->flush();
         }
         $io->text('Reset [OK]');
+    }
+
+    public function getEntityManagerWindev(): ObjectManager
+    {
+        return $this->emWindev;
+    }
+
+    public function getEntityManager(): ObjectManager
+    {
+        return $this->em;
     }
 }
